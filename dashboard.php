c941b2bf3e3bd6209bbc0070d663f810b1e60dac
Stig11686 <42617540+Stig11686@users.noreply.github.com>
2019-07-15 08:38:57 +0100

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+$userid = $_POST["userid"];
+$password = $_POST["password"];
+
+if($userid == "GTCoding" && $password == "123456"){
+    echo "<h1>Welcome, $userid</h1>";
+} else {
+    echo "Incorrect Login Details";
+}
+
+
+?>
